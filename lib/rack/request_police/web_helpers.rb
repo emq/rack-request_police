a0d9ab638dd39a4c040a3bb776863603fe018142
Rafal Wojsznis <rafal.wojsznis@gmail.com>
2015-02-04 22:36:37 +0100

    Hacking more views
    ...also stealing more code from sidekiq's source (web/) <3

--- lib/rack/request_police/web_helpers.rb
+++ lib/rack/request_police/web_helpers.rb
@@ -13,6 +13,22 @@ module Rack
           'danger'
         end
       end
+
+      def environment_name
+        environment = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
+        "[#{environment.upcase}]"
+      end
+
+      def qparams(options)
+        options = options.stringify_keys
+        params.merge(options).map do |key, value|
+          "#{key}=#{value}"
+        end.join("&")
+      end
+
+      def root_path
+        "#{env['SCRIPT_NAME']}/"
+      end
     end
   end
 end
